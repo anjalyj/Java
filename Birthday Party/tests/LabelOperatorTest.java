dd0c62c985929fe1a9305a015a2897c17df27e25
anjalyj <anjalyj@thoughtworks.com>
2016-03-04 18:23:24 +0530

    Added more Tests to check the the gender enum is working properly.

--- Birthday Party/tests/LabelOperatorTest.java
+++ Birthday Party/tests/LabelOperatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 public class LabelOperatorTest {
     @Test
-    public void gives_the_name_in_the_format_first_name_first() throws Exception {
+    public void gives_the_name_in_the_format_first_name_first_for_male() throws Exception {
         Name name = new Name("John", "Doe");
         Address address = new Address("Veda haven", "Vermont", "Macedonia");
         Person person = new Person(name, Gender.Male, 25, address);
@@ -15,7 +15,18 @@ public class LabelOperatorTest {
     }
 
     @Test
-    public void gives_the_name_in_the_format_last_name_first() throws Exception {
+    public void gives_the_name_in_the_format_first_name_first_for_female() throws Exception {
+        Name name = new Name("Melody", "Dooley");
+        Address address = new Address("Veda haven", "Vermont", "Macedonia");
+        Person person = new Person(name, Gender.Female, 25, address);
+        LabelOperator op = new LabelOperator(person);
+        String expected = "Ms Melody Dooley";
+        String actual = op.getFirstNameFirst();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gives_the_name_in_the_format_last_name_first_for_male() throws Exception {
         Name name = new Name("John", "Doe");
         Address address = new Address("Veda haven", "Vermont", "Macedonia");
         Person person = new Person(name, Gender.Male, 25, address);
@@ -26,7 +37,17 @@ public class LabelOperatorTest {
     }
 
     @Test
-    public void gives_the_name_in_the_format_first_name_first_with_country() throws Exception {
+    public void gives_the_name_in_the_format_last_name_first_for_female() throws Exception {
+        Name name = new Name("Melody", "Dooley");
+        Address address = new Address("Veda haven", "Vermont", "Macedonia");
+        Person person = new Person(name, Gender.Female, 25, address);
+        LabelOperator op = new LabelOperator(person);
+        String expected = "Ms Dooley, Melody";
+        String actual = op.getFirstNameLast();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void gives_the_name_in_the_format_first_name_first_with_country_for_male() throws Exception {
         Name name = new Name("John", "Doe");
         Address address = new Address("Veda haven", "Vermont", "Macedonia");
         Person person = new Person(name, Gender.Male, 25, address);
@@ -37,12 +58,34 @@ public class LabelOperatorTest {
     }
 
     @Test
-    public void gives_the_name_in_the_format_last_name_first_with_country() throws Exception {
+    public void gives_the_name_in_the_format_first_name_first_with_country_female() throws Exception {
+        Name name = new Name("Melody", "Dooley");
+        Address address = new Address("Veda haven", "Vermont", "Macedonia");
+        Person person = new Person(name, Gender.Female, 25, address);
+        LabelOperator op = new LabelOperator(person);
+        String expected = "Ms Melody Dooley, Macedonia";
+        String actual = op.getFirstNameFirstWithCountry();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gives_the_name_in_the_format_last_name_first_with_country_for_male() throws Exception {
         Name name = new Name("John", "Doe");
         Address address = new Address("Veda haven", "Vermont", "Macedonia");
         Person person = new Person(name, Gender.Male, 25, address);
         LabelOperator op = new LabelOperator(person);
         String expected = "Mr Doe, John, Macedonia";
+        String actual = op.getFirstNameLastWithCountry();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gives_the_name_in_the_format_last_name_first_with_country_for_female() throws Exception {
+        Name name = new Name("Melody", "Dooley");
+        Address address = new Address("Veda haven", "Vermont", "Macedonia");
+        Person person = new Person(name, Gender.Female, 25, address);
+        LabelOperator op = new LabelOperator(person);
+        String expected = "Ms Dooley, Melody, Macedonia";
         String actual = op.getFirstNameLastWithCountry();
         assertEquals(expected, actual);
     }
